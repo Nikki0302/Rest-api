2d57854e01861fead93582cf0e647e368e3a177a
Nikki0302 <148523153+Nikki0302@users.noreply.github.com>
2023-11-09 11:01:45 +0800

    Update user.route.js

--- user.route.js
+++ user.route.js
@@ -1,8 +1,13 @@
-import {Router} from "express";
-import comtroller from "./user.controller.js";
-
+import { Router } from "express"
+import controller from "./user.controller.js";
 const userRouter = Router();
 
+userRouter.get("/:Id", controller.get);
 userRouter.get("/", controller.get);
+
+userRouter.post("/", controller.post);
+userRouter.put("/:Id", controller.put);
+
+userRouter.delete("/:Id", controller.delete);
 
 export default userRouter;
